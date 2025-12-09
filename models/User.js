57ce45ee32ca8f6b9331061efa09b10b9cf61acb
mariaramynabil <mariaramyofficialacc@gmail.com>
2025-12-09 16:34:00 +0200

    Update User.js

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength: [8, 'Password must be at least 8 characters'],
+    select: false, // Don't return password in queries by default
+  },
+});
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next(); // Only hash if changed
+  this.password = await bcrypt.hash(this.password, 12); // 12 salt rounds
+  next();
+});
+
+// Instance method to compare passwords
+userSchema.methods.correctPassword = async function (candidatePassword, userPassword) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
